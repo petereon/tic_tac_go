d9465c4f7da46759d6c9f525fb1bae5d3ae374a7
petereon <pvyboch1@gmail.com>
2023-05-11 22:55:17 +0200

    feat: place mark on board

--- src/tic_tac_toe/tic_tac_toe.go
+++ src/tic_tac_toe/tic_tac_toe.go
@@ -1,7 +1,17 @@
 package tic_tac_toe
 
+import "fmt"
+
 type Board []string
 
 func CreateBoard() Board {
 	return Board{" ", " ", " ", " ", " ", " ", " ", " ", " "}
+}
+
+func (board Board) PlaceMark(position int, mark string) (Board, error) {
+	if board[position] != " " {
+		return board, fmt.Errorf("position already taken")
+	}
+	board[position] = mark
+	return board, nil
 }
--- test/tic_tac_toe_test.go
+++ test/tic_tac_toe_test.go
@@ -12,3 +12,31 @@ func TestCreateBoard(t *testing.T) {
 		t.Error("Expected board to have 9 spaces")
 	}
 }
+
+func TestPlaceMark(t *testing.T) {
+	board := tic_tac_toe.CreateBoard()
+	board, err := board.PlaceMark(0, "X")
+	if err != nil {
+		t.Error("Expected no error")
+	}
+	if board[0] != "X" {
+		t.Error("Expected board to have X at position 0")
+	}
+	for _, v := range board[1:] {
+		if v != " " {
+			t.Error("Expected board to have empty spaces")
+		}
+	}
+}
+
+func TestPlaceMarkFails(t *testing.T) {
+	board := tic_tac_toe.CreateBoard()
+	board, _ = board.PlaceMark(0, "X")
+	board, err := board.PlaceMark(0, "O")
+	if err == nil {
+		t.Error("Expected error")
+	}
+	if board[0] == "O" {
+		t.Error("Expected board to have X at position 0")
+	}
+}
